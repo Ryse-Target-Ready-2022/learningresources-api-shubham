7a242f145790ef614090ac75f4b3c54bd21b1420
Shubham <shubhampandiya2000@gmail.com>
2022-07-26 18:52:37 +0530

    ServiceTests updated and all tests passed

--- src/test/java/com/tgt/rysetii/learningresourcesapi/service/LearningResourceServiceTest.java
+++ src/test/java/com/tgt/rysetii/learningresourcesapi/service/LearningResourceServiceTest.java
@@ -13,6 +13,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -30,7 +31,7 @@ class LearningResourceServiceTest {
     }
 
     @Test
-    void saveLearningResources() {
+    public void saveLearningResources() {
         List<LearningResource> list = new ArrayList<>();
         LearningResource ls1 = new LearningResource(1311, "Test Name 1", 100.0, 120.0, LearningResourceStatus.LIVE, LocalDate.now(), LocalDate.now().plusMonths(5), LocalDate.now().plusYears(2));
         LearningResource ls2 = new LearningResource(1312, "Test Name 2", 120.0, 180.0, LearningResourceStatus.LIVE, LocalDate.now(), LocalDate.now().plusMonths(6), LocalDate.now().plusYears(3));
@@ -44,14 +45,47 @@ class LearningResourceServiceTest {
     }
 
     @Test
-    void getProfitMargin() {
+    public void getProfitMargin() {
+        List<LearningResource> learningResourcesList = new ArrayList<>();
+        LearningResource learningResource1 = new LearningResource(1311, "Test Name 1", 100.0, 120.0, LearningResourceStatus.LIVE, LocalDate.now(), LocalDate.now().plusMonths(5), LocalDate.now().plusYears(2));
+        LearningResource learningResource2 = new LearningResource(1312, "Test Name 2", 120.0, 180.0, LearningResourceStatus.LIVE, LocalDate.now(), LocalDate.now().plusMonths(6), LocalDate.now().plusYears(3));
+        learningResourcesList.add(learningResource1);
+        learningResourcesList.add(learningResource2);
+
+        List<Double> expectedProfitMargins = learningResourcesList.stream()
+                .map(lr->(lr.getSellingPrice()-lr.getCostPrice()/lr.getSellingPrice()))
+                .collect(toList());
+
+        when(repository.findAll()).thenReturn(learningResourcesList);
+
+        List<Double> actualProfitMargins = service.getProfitMargin();
+        assertEquals(expectedProfitMargins,actualProfitMargins, "Wrong profit margins");
     }
 
     @Test
-    void sortLearningResourcesByProfitMargin() {
+    public void sortLearningResourcesByProfitMargin() {
+        List<LearningResource> sortedList = new ArrayList<>();
+        LearningResource learningResource1 = new LearningResource(1311, "Test Name 1", 100.0, 120.0, LearningResourceStatus.LIVE, LocalDate.now(), LocalDate.now().plusMonths(5), LocalDate.now().plusYears(2));
+        LearningResource learningResource2 = new LearningResource(1312, "Test Name 2", 120.0, 180.0, LearningResourceStatus.LIVE, LocalDate.now(), LocalDate.now().plusMonths(6), LocalDate.now().plusYears(3));
+        sortedList.add(learningResource1);
+        sortedList.add(learningResource2);
+
+        sortedList.sort((lr1, lr2) -> {
+            Double profitMargin1 = (lr1.getSellingPrice() - lr1.getCostPrice())/lr1.getSellingPrice();
+            Double profitMargin2 = (lr2.getSellingPrice() - lr2.getCostPrice())/lr2.getSellingPrice();
+
+            return profitMargin2.compareTo(profitMargin1) ;
+        });
+
+        when(repository.findAll()).thenReturn(sortedList);
+        List<LearningResource> actualSrotedList = service.sortLearningResourcesByProfitMargin();
+        assertEquals(sortedList, actualSrotedList, "Wrongly sorted");
     }
 
     @Test
-    void deleteLearningResourceById() {
+    public void deleteLearningResourceById() {
+        int learningResourceId = 1234;
+        service.deleteLearningResourceById(learningResourceId);
+        verify(repository, times(1)).deleteById(learningResourceId);
     }
 }
