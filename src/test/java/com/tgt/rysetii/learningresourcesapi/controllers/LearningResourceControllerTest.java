42562d7b58ad2f748d369535725924e94b76e179
Shubham <shubhampandiya2000@gmail.com>
2022-07-26 19:18:11 +0530

    created test class for controller

---
+++ src/test/java/com/tgt/rysetii/learningresourcesapi/controllers/LearningResourceControllerTest.java
@@ -0,0 +1,20 @@
+package com.tgt.rysetii.learningresourcesapi.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LearningResourceControllerTest {
+
+    @Test
+    public void handleGetAll() {
+    }
+
+    @Test
+    public void saveLearningResources() {
+    }
+
+    @Test
+    public void deleteLearningResources() {
+    }
+}
